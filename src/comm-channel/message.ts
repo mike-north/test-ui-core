3b8d9b9b01303047990109512d8bb9504e1b7143
Mike North <michael.l.north@gmail.com>
2018-07-26 23:28:22 -0700

    fix: remove pointless function

--- src/comm-channel/message.ts
+++ src/comm-channel/message.ts
@@ -2,14 +2,10 @@ import { Observable, filter, fromEvent, map } from 'micro-observable';
 import CommChannel from '../comm-channel';
 import { Data } from '../types';
 
-function messageEventToData(me: MessageEvent): Data {
-  return me.data;
-}
-
 export default class MessageCommChannel extends CommChannel<Data> {
   private obs: Observable<any> = fromEvent<'message'>(window, 'message').pipe(
     filter(evt => evt.data._testFrame !== void 0),
-    map(messageEventToData)
+    map(me => me.data)
   );
   async setup(): Promise<void> {
     this.obs.subscribe(evt => {
